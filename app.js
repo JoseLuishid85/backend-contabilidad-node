76de5401e1bbdda35002e3f2ee34db7d28de76c0
Jose Luis Hidalgo <joseluishid01@gmail.com>
2026-04-15 15:42:25 -0400

    Prueba 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config({ quiet: true });
 const sequelize = require('./config/database');
-const setupAssociations = require('./models/associations');
+//const setupAssociations = require('./models/associations');
 
 const app = express();
 
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-setupAssociations();
+//setupAssociations();
 
 // Database Sync
 sequelize.sync({ alter: false })
@@ -21,8 +21,8 @@ sequelize.sync({ alter: false })
 // Routes
 app.use('/accounting/api/login', require('./routes/authRouter.js'));
 app.use('/accounting/api/users', require('./routes/userRouter.js'));
-app.use('/accounting/api/suppliers', require('./routes/supplierRouter.js'));
-app.use('/accounting/api/purchases', require('./routes/purchaseRouter.js'));
+//app.use('/accounting/api/suppliers', require('./routes/supplierRouter.js'));
+//app.use('/accounting/api/purchases', require('./routes/purchaseRouter.js'));
 
 const PORT = process.env.PORT || 4000;
 
